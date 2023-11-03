a0ab113f085c3963727af7bc97cef2b3479f55c8
M. Antonella Tesio <antotesio@gmail.com>
2023-11-02 21:33:17 -0300

    perro

---
+++ prueba_git/src/prueba_git/Perro.java
@@ -0,0 +1,13 @@
+
+package prueba_git;
+
+/**
+ *
+ * @author antot
+ */
+public class Perro {
+    private String nombre;
+    private Boolean cola;
+    private String color;
+    private Integer edad;
+}
