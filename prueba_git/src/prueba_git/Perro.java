58f462990accf4e394ff2d0e01dd1cc47a97e33f
M. Antonella Tesio <antotesio@gmail.com>
2023-11-02 21:38:50 -0300

    perro

---
+++ prueba_git/src/prueba_git/Perro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_git;
+
+/**
+ *
+ * @author antot
+ */
+public class Perro {
+    private String nombre;
+    private Boolean cola;
+    private Integer edad;
+    private String color;
+}
